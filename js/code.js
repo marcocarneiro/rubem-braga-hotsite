f3458ebc2dd5a7192ce6a77e9374d8bd5ad0aa26
marcocarneiro <marco.carneiro7@gmail.com>
2022-08-30 11:59:52 -0300

    Códigos JS

--- js/code.js
+++ js/code.js
@@ -1,2 +1,34 @@
 //Códigos para o Hot site Rubem Braga
 
+//Cria um novo objeto da classe Date
+const tempo = new Date()
+const txtData = tempo.getFullYear() + ' - hora: ' + tempo.getHours() + ' minuto: ' + tempo.getMinutes()
+
+//ELEMENTOS DOM
+const spanData = document.getElementById('data')
+const btFechar = document.getElementById('btFechar')
+const divModal = document.getElementById('divModal')
+const btHamburguer = document.getElementById('btHamburguer')
+const navPhone = document.getElementById('navPhone')
+const btFecharMenu = document.addEventListener('btFecharMenu')
+
+//Insere txtData dentro do ID "data"
+spanData.innerText = txtData
+
+
+//AÇÕES DO USUÁRIO (eventos)
+
+//Ao clicar no id "btFechar"
+btFechar.addEventListener('click', function(){
+    //oculta o divModal -  CSS display valor none (por javascript)
+    divModal.style.display = 'none'
+})
+    
+
+btHamburguer.addEventListener('click', function(){
+    navPhone.style.display = 'block'
+})
+
+btFecharMenu.addEventListener('click', function(){
+    navPhone.style.display = 'none'
+})
